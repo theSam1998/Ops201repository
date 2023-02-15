d72fa7cb26f7724e253fc200df41b0d44e222173
theSam1998 <2894sallan@gmail.com>
2023-02-15 23:26:05 +0000

    trying to understand lab02

--- practice.sh
+++ practice.sh
@@ -1,3 +1,12 @@
+#!/bin/bash
+
 # this is some text
 # this is some more text
 # I am just testing things, Having trouble grasping this one.
+# I am testing changes now here in vscode
+#adding this new text in vscode and going to push to github.
+
+# Main
+echo "I finally got it!"
+echo "isn't my server name cool:"
+hostname
