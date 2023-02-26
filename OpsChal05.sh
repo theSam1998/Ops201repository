9075bc6b594b272abf430279f9ae68020e68f28e
Samuel Allan <123232378+theSam1998@users.noreply.github.com>
2023-02-25 19:11:07 -0800

    Update OpsChal05.sh
    fixed incorrect comments. had some trouble pushing this one initially

--- OpsChal05.sh
+++ OpsChal05.sh
@@ -11,15 +11,19 @@ ps aux
 echo "these are the running processes"
 #prompting the user for the PID and storing the input in a variable called killit
 read -p "what PID would you like me to kill" killit
-# if the PID entered is a running program, then kill it and prompt the user
+# checks if the input is a running PID
 if ps -p $killit > /dev/null; then
     while [[ "$yoohoo" =~ ^([yY][eE][sS]|[yY])$ ]]; do
+#kill the program
     kill $killit
+#wanna do it again? stores input in the variable yoohoo
     read -p "PID Terminated. Any more boss? (y/n)" yoohoo
+#if yes, starts over from the prompt for the PID
         if [[ "$yoohoo" =~ ^([yY][eE][sS]|[yY])$ ]]; then
             read -p "what PID would you like me to kill" killit
         fi
     done
+#but otherwise...
 else
     echo "sorry, can't do a thing with that. Try again"
 fi
